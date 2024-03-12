91eae7032d18a330938e44fd7d461598f82b7754
Belle Akemi Tamura <121132188+belleakemi@users.noreply.github.com>
2024-03-11 21:56:58 -0300

    analise_nanosight.R

--- analise_nanosight.R
+++ analise_nanosight.R
@@ -4,11 +4,11 @@ library(dplyr)
 library(readr)
 library(broom)
 #Definir pasta de trabalho e importar scripts necessários
-setwd("C:/Users/Belle/Documents/NTA")
+setwd("C:/Users/Belle/Documents/Belle")
 source("gera_pdf_qq_csv_norm.R")
 source("analise_nanosight_utils.R")
 #Trazer tabelas de interesse para o R
-meuwd <- setwd ("C:/Users/Belle/Documents/NTA")
+meuwd <- setwd ("C:/Users/Belle/Documents/Belle")
 isolar_exp_sum <- list.files(pattern="*ExperimentSummary.csv") 
 importar_exp_sum <- lapply(isolar_exp_sum, read.csv, header = FALSE, col.names = c(paste0("V", 1:6))) #importa todas as expsum corretamente ao nomear as colunas
 #Extrair dados de interesse de ExpSum
@@ -40,52 +40,44 @@ na_indexes_tamanho_mode <- which(is.na(tamanho_mode_average))
 tamanho_mode_average_alt <- tamanho_mode_average_alt[na_indexes_tamanho_mode]
 tamanho_mode_average[is.na(tamanho_mode_average)] <- tamanho_mode_average_alt
 #Juntando todos os dados obtidos do nanosight em uma tabela
-concentracao_real <- concentracao_real[-c(139:143,146)] #ficar de olho pra ver se essas pos nao mudam conforme add amostra e qqr coisa atualizar os numeros
-tamanho_mean_average <- tamanho_mean_average[-c(139:143,146)] #ficar de olho pra ver se essas pos nao mudam conforme add amostra e qqr coisa atualizar os numeros
-tamanho_mode_average <- tamanho_mode_average[-c(139:143,146)] #ficar de olho pra ver se essas pos nao mudam conforme add amostra e qqr coisa atualizar os numeros
 tabela_tudo_nanosight <- data.frame(id_sample, concentracao_real, tamanho_mean_average, tamanho_mode_average)
-tabela_tudo_nanosight<- na.omit(tabela_tudo_nanosight) #amostra 10521_w2 tinha uma leitura a mais sem valores
+duplicatas <- duplicated(tabela_tudo_nanosight[, c("id_sample")], fromLast = TRUE) #a 2a leitura é a correta
+nanosight_sem_duplicatas <- subset(tabela_tudo_nanosight, !duplicatas)
 #Juntar dados do nanosight com informações da amostra
 sample_information <- read.csv("sampleinformation_liriel320_atualizada.csv") #chamando/lendo/importando infos da amostra
 sample_information$id_sample <- paste(sample_information$subjectid, sample_information$wave, sep = "_")
-nanosight_plus_sampleinfo <- inner_join(tabela_tudo_nanosight, sample_information, by="id_sample")
-anti_join(tabela_tudo_nanosight, nanosight_plus_sampleinfo, by = "id_sample") #6 amostras sem correspondência
+nanosight_plus_sampleinfo <- inner_join(nanosight_sem_duplicatas, sample_information, by="id_sample")
+sem_correspondencia <- anti_join(nanosight_sem_duplicatas, sample_information, by = "id_sample")
 write.csv(nanosight_plus_sampleinfo, "nanosight_plus_sampleinfo_all.csv", row.names = FALSE)
-###Para selecionar as amostras dentro do zscore entre -1.5 e +1.5:
-nanosight_plus_sampleinfo$zscore_mean <- scale(nanosight_plus_sampleinfo$tamanho_mean_average)
-nanosight_plus_sampleinfo$zscore_mode <- scale(nanosight_plus_sampleinfo$tamanho_mode_average)
-nanosight_plus_sampleinfo$zscore_concentracao <- scale(nanosight_plus_sampleinfo$concentracao_real)
-nanosight_sem_outliers <- nanosight_plus_sampleinfo[nanosight_plus_sampleinfo$zscore_mean >= -1.5 & nanosight_plus_sampleinfo$zscore_mean <= 1.5 &
-                                                  nanosight_plus_sampleinfo$zscore_mode >= -1.5 & nanosight_plus_sampleinfo$zscore_mode <= 1.5 &
-                                                  nanosight_plus_sampleinfo$zscore_concentracao >= -1.5 & nanosight_plus_sampleinfo$zscore_concentracao <= 1.5, ]
-outliers <- anti_join(nanosight_plus_sampleinfo, nanosight_sem_outliers, by = "id_sample") #26 outliers
-###Testes de normalidade
-shapiro_mean <- shapiro.test(nanosight_sem_outliers$tamanho_mean_average)
-shapiro_mode <- shapiro.test(nanosight_sem_outliers$tamanho_mode_average)
-shapiro_concentracao <- shapiro.test(nanosight_sem_outliers$concentracao_real)
-shapiro_testes <- data.frame(mean = c(shapiro_mean), mode = c(shapiro_mode), concentracao = c(shapiro_concentracao))
-shapiro_testes <- select(shapiro_testes, mean.p.value, mode.p.value, concentracao.p.value)
-###QQPlot
-dados_mean <- rnorm(nanosight_sem_outliers$tamanho_mean_average)
+#Amostras sem análise no Nanosight
+sample_info_sem_nanosight <- anti_join(sample_information, nanosight_sem_duplicatas, by="id_sample")
+#QQPlot para analisar se as amostras seguem uma distribuição normal
+dados_mean <- rnorm(nanosight_plus_sampleinfo$tamanho_mean_average)
 pdf("qqplot_mean.pdf")
 qqnorm(dados_mean)
 qqline(dados_mean, col = "red")
 dev.off()
-dados_mode <- rnorm(nanosight_sem_outliers$tamanho_mode_average)
+dados_mode <- rnorm(nanosight_plus_sampleinfo$tamanho_mode_average)
 pdf("qqplot_mode.pdf")
 qqnorm(dados_mode)
 qqline(dados_mode, col = "red")
 dev.off()
+###Testes de Shapiro
+shapiro_mean <- shapiro.test(nanosight_plus_sampleinfo$tamanho_mean_average)
+shapiro_mode <- shapiro.test(nanosight_plus_sampleinfo$tamanho_mode_average)
+shapiro_concentracao <- shapiro.test(nanosight_plus_sampleinfo$concentracao_real)
+shapiro_testes <- data.frame(mean = c(shapiro_mean), mode = c(shapiro_mode), concentracao = c(shapiro_concentracao))
+shapiro_testes <- select(shapiro_testes, mean.p.value, mode.p.value, concentracao.p.value)
 #Contando número de amostras dos grupos e waves
 library(stringr)
-numero_amostras_grupos <- nanosight_sem_outliers %>%
+numero_amostras_grupos <- nanosight_plus_sampleinfo %>%
   group_by(Trajetoria) %>%
   summarize(Amostras = n(),)
-numero_amostras_grupos_w1 <- nanosight_sem_outliers %>%
+numero_amostras_grupos_w1 <- nanosight_plus_sampleinfo %>%
   filter(Trajetoria %in% c("A", "B", "C", "D") & str_detect(wave, "w1")) %>%
   group_by(Trajetoria) %>%
   summarise(w1 = n())
-numero_amostras_grupos_w2 <- nanosight_sem_outliers %>%
+numero_amostras_grupos_w2 <- nanosight_plus_sampleinfo %>%
   filter(Trajetoria %in% c("A", "B", "C", "D") & str_detect(wave, "w2")) %>%
   group_by(Trajetoria) %>%
   summarise(w2 = n())
@@ -93,35 +85,34 @@ numero_amostras_grupos_waves <- numero_amostras_grupos %>%
   inner_join(numero_amostras_grupos_w1, by = "Trajetoria") %>%
   inner_join(numero_amostras_grupos_w2, by = "Trajetoria")
 write.csv(numero_amostras_grupos_waves, "amostras_grupos_waves.csv", row.names = FALSE)
-#Contando numero de amostras dos grupos e waves dos outliers
-outliers_grupos <- outliers %>%
-  group_by(Trajetoria) %>%
-  summarize(Amostras = n(),)
-outliers_grupos_w1 <- outliers %>%
-  filter(Trajetoria %in% c("A", "B", "C", "D") & str_detect(wave, "w1")) %>%
-  group_by(Trajetoria) %>%
-  summarise(w1 = n())
-outliers_grupos_w2 <- outliers %>%
-  filter(Trajetoria %in% c("A", "B", "C", "D") & str_detect(wave, "w2")) %>%
-  group_by(Trajetoria) %>%
-  summarise(w2 = n())
-outliers_grupos_waves <- outliers_grupos %>%
-  inner_join(outliers_grupos_w1, by = "Trajetoria") %>%
-  inner_join(outliers_grupos_w2, by = "Trajetoria")
-write.csv(outliers_grupos_waves, "outliers_grupos_waves.csv", row.names = FALSE)
-
-##########PAREI AQUI EM 05/09/23#################
-#Separar w1 de w2
-tabela_w1 <- nanosight_sem_outliers[nanosight_sem_outliers$wave == "w1",]
-write.csv(tabela_w1, "nanosight_sem_outliers_w1.csv", row.names = FALSE)
-tabela_w2 <- nanosight_sem_outliers[nanosight_sem_outliers$wave == "w2",]
-write.csv(tabela_w2, "nanosight_sem_outliers_w2.csv", row.names = FALSE)
-##Separar grupos
-grupo_persistentes <- nanosight_sem_outliers[nanosight_sem_outliers$Trajetoria == "A",]
-write.csv(grupo_persistentes, "nanosight_sem_outliers_persistentes.csv", row.names = FALSE)
-grupo_incidentes <- nanosight_sem_outliers[nanosight_sem_outliers$Trajetoria == "B",]
-write.csv(grupo_incidentes, "nanosight_sem_outliers_incidentes.csv", row.names = FALSE)
-grupo_remitentes <- nanosight_sem_outliers[nanosight_sem_outliers$Trajetoria == "C",]
-write.csv(grupo_remitentes, "nanosight_sem_outliers_remitentes.csv", row.names = FALSE)
-grupo_controles <- nanosight_sem_outliers[nanosight_sem_outliers$Trajetoria == "D",]
-write.csv(grupo_controles, "nanosight_sem_outliers_controles.csv", row.names = FALSE)
+#Amostras sem pares
+linhas_sem_pares <- !duplicated(nanosight_plus_sampleinfo$subjectid) & !duplicated(nanosight_plus_sampleinfo$subjectid, fromLast = TRUE)
+nanosight_sem_pares <- nanosight_plus_sampleinfo[linhas_sem_pares, ]
+#Boxplot da Média dos Tamanhos
+library(ggplot2)
+boxplot_tamanho_mean <- ggplot(nanosight_plus_sampleinfo, aes(x = Trajetoria, y = tamanho_mean_average)) +
+  geom_boxplot(fill = "#DDA0DD", color = "#800080", alpha = 0.7) +
+  labs(title = "Mean Size Boxplot by Group", x = "Group", y = "Mean size (nm)") +
+  theme_classic()
+ggsave("boxplot_tamanho_mean.pdf", plot = boxplot_tamanho_mean + theme(text = element_text(size=20)), width = 8, height = 6)
+boxplot_tamanho_mean_w1 <- ggplot(subset(nanosight_plus_sampleinfo, wave == "w1"), aes(x = Trajetoria, y = tamanho_mean_average)) +
+  geom_boxplot(fill = "#DDA0DD", color = "#800080", alpha = 0.7) +
+  labs(title = "Mean Size Boxplot for Category w1", x = "Group", y = "Mean size (nm)") +
+  theme_classic()
+ggsave("boxplot_tamanho_mean_w1.pdf", plot = boxplot_tamanho_mean_w1 + theme(text = element_text(size=20)), width = 8, height = 6)
+boxplot_tamanho_mean_w2 <- ggplot(subset(nanosight_plus_sampleinfo, wave == "w2"), aes(x = Trajetoria, y = tamanho_mean_average)) +
+  geom_boxplot(fill = "#DDA0DD", color = "#800080", alpha = 0.7) +
+  labs(title = "Mean Size Boxplot for Category w2", x = "Group", y = "Mean size (nm)") +
+  theme_classic()
+ggsave("boxplot_tamanho_mean_w2.pdf", plot = boxplot_tamanho_mean_w2 + theme(text = element_text(size=20)), width = 8, height = 6)
+#Teste LSD
+library(lmerTest)
+library(lsmeans)
+modelo_anova <- lmer(tamanho_mean_average ~ Trajetoria + (1|Trajetoria), data = nanosight_plus_sampleinfo)
+resultado_anova <- anova(modelo_anova)
+if (resultado_anova$`Pr(>F)`[1] < 0.05) {
+  resultado_lsd <- lsmeans::lstrends(modelo_anova, ~Trajetoria)
+  print(resultado_lsd)
+} else {
+  print("O teste ANOVA não é significativo. Não é apropriado realizar o teste LSD.")
+}
